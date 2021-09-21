5ef82643236e15c4d28c496bbdf75553277f04be
parkerdouglass <parker.douglass@icloud.com>
2021-09-21 01:40:55 -0700

    Did the skeleton for the front page of the documentation

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,78 @@
-<h1>chattter</h1>
-<h4>site coming soon</h4>
+<div class="hero">
+    <div class="hero-text">
+        <h1>The open source choice for team communication</h1>
+    </div>
+    <div class=hero-subtext>
+        <div class="card-display">
+            <div class="card-container">
+              <Card padded flat style="background-color: #313131;">https://github.com/chattter/</Card>
+            </div>
+            <a href="/" class = "doc-button">View Documentation</a>
+    </div>
+</div>
+</div>
+<div class="page-content">
+    <p>
+        chattter is a completely free and open-source application that allows you to easily create a way for
+        your entire team to converse about your current and future projects
+    </p>
+    <p class="list">
+        <span class="check">✔</span> Open-Source <br>
+        <span class="check">✔</span> Fast <br>
+        <span class="check">✔</span> Up-to-Date Documentation <br>
+        <span class="check">✔</span> Ease of Use <br>
+
+    </p>
+    </div>
+<style>
+    .hero {
+        background-color: #272727;
+        color: white;
+        padding: 100px;
+        text-align: center;
+        margin:0px;
+    }
+    .hero-subtext{
+        background-color: #282828;
+    }
+    .card-container{
+        color: white;
+        margin-left: 250px;
+        margin-right: 250px;
+        padding-bottom: 40px;
+    }
+    .doc-button{
+        color: white;
+        text-decoration: none;
+        margin-top:-1000px;
+        border: 1px solid #3aad3c;
+        padding: 10px;
+        background-color: #3aad3c;
+        border-radius: 2px;
+    }
+    .page-content{
+        background-color: #313131;
+        color: white;
+        height: 100%;
+        padding: 50px;
+        margin: 0px;
+        text-align: left;
+        font-size: 20px;
+        line-height: 2em;
+        padding-left: 100px;
+    }
+    .list{
+        padding-top:10px;
+        text-align:left;
+        padding-left:20px;
+        line-height: 2.5em;
+    }
+    .check{
+        color: #0ccc0f;
+    }
+</style>
+
+<script lang="ts">
+    import Card, { Content } from '@smui/card';
+    import IconButton, { Icon } from '@smui/icon-button';
+  </script>
